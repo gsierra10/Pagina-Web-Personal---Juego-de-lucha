7333fae017b62433ae8426e2f68a5ecc850de323
GustavoSierra <gsierra.business@gmail.com>
2021-10-18 23:55:50 +0200

    agregadas funciones de seleccion y manipulacion del dom

--- juego.js
+++ juego.js
@@ -7,10 +7,35 @@ var player1 = document.getElementById('player1-vida');
 var player1Hp = document.getElementsByClassName('player1-hp');
 var player2Hp = document.getElementsByClassName('player2-hp');
 var gameOver = document.getElementById('game-over'); 
+var players = document.querySelectorAll('#players>div')
+let playerSelected = []
+let startBtn = document.querySelector('.start-button')
+let vidaDe1 = document.querySelector('.vida-label1')
+let vidaDe2 = document.querySelector('.vida-label2')
 
 //Variables de vida//
 var playerHp = 100;
 var secondPlayerHp = 100;
+
+function selectPlayer(x){
+    console.log(x.classList.value)
+    if (!playerSelected.includes(x.classList.value)){
+        if(playerSelected.length < 2){
+            playerSelected.push(x.classList.value)
+            console.log(playerSelected)
+            if (playerSelected.length === 2) {
+                startBtn.disabled = false;
+                bottomRow.querySelector('p').innerHTML = 'Has seleccionado a ' +playerSelected[0]  + ' y a ' + playerSelected[1] + ' para que luchen.'
+            }    
+        } else{
+            console.log('Too many players selected')
+            console.log(playerSelected)
+        } 
+    }else {
+        console.log('already selected')
+    }
+    }
+
 
 
 function start(){
@@ -18,6 +43,13 @@ function start(){
     for(let x=0; x<vida.length; x++){
         vida[x].style.visibility = "visible";
     }
+    players.forEach(player => {
+        if (!playerSelected.includes(player.classList.value)) {
+            player.style.display = "none";
+        }
+    });
+    vidaDe1.innerHTML = "Vida de " + playerSelected[0];
+    vidaDe2.innerHTML = "Vida de " + playerSelected[1]; 
 }
 
 function ataque1(){
@@ -28,7 +60,7 @@ function ataque1(){
         if(secondPlayerHp < 0){
             secondPlayerHp = 0;
         }
-        bottomRow.innerHTML = "Le has quitado al enemigo " + dmg + " de daño. Al Player 2 le queda " + secondPlayerHp + " de vida";
+        bottomRow.innerHTML = "Le has quitado al enemigo " + dmg + " de daño. A " + playerSelected[1] + " le queda " + secondPlayerHp + " de vida";
         var secondPlayerBarHp = (secondPlayerHp/100)*295;
         player2Hp[0].style.width = secondPlayerBarHp + "px";
     } else{
@@ -48,7 +80,7 @@ function ataque2(){
         if(playerHp < 0){
             playerHp = 0;
         }
-        bottomRow.innerHTML = "Le has quitado al enemigo " + dmg + " de daño. Al Player 1 le queda " + playerHp + " de vida";
+        bottomRow.innerHTML = "Le has quitado al enemigo " + dmg + " de daño. A " + playerSelected[0] + " le queda " + playerHp + " de vida";
         var PlayerBarHp = (playerHp/100)*295;
         player1Hp[0].style.width = PlayerBarHp + "px";
     } else{
